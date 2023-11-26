5fbc31e6bfe0e9f532511d3caf8c51e7fe413640
HannesHK <hannes.kisel@gmail.com>
2023-11-26 17:20:20 +0200

    stuff1

---
+++ src/components/Footer.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="footer">
+        <p>2023 WAD. All rights reserved</p>
+    </div>
+</template>
+
+<script>
+export default  {
+    name:"footer"
+}
+</script>
+
+<style scoped>
+
+</style>
