7212f045953709bb950595f506db73d88e996881
HannesHK <hannes.kisel@gmail.com>
2023-11-26 17:02:59 +0200

    stuff

---
+++ src/components/Header.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="header">
+        <nav>
+            <router-link to="/">Home</router-link> |
+            <router-link to="/signUp">signUp</router-link>
+        </nav>
+    </div>
+</template>
+
+<script>
+export default  {
+    name:"header"
+}
+</script>
+
+<style scoped>
+
+</style>
